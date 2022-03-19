fd23b889eab42bfd49f47a3f61c42253dfe5997a
WaSiKoRn <101938202+WaSiKoRn@users.noreply.github.com>
2022-03-19 22:06:47 +0700

    อัปโหลดไฟล์เต่าวาดสี่เหลี่ยม

---
+++ Z2.py
@@ -0,0 +1,19 @@
+import turtle
+tao = turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+
+def Rectangle():
+    for i in range(4):
+        tao.forward(100)
+        tao.left(90)
+
+
+def Go(x,y):
+    tao.penup()
+    tao.goto(x,y)
+    tao.pendown()
+
+
+Go(200,200)
+Rectangle()
